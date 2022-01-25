a8cbb80c5deff8d014c76e24d3a22fe1d3be172d
unknown <selinankunas@gmail.com>
2022-01-25 12:03:35 +0200

    if added

--- login.js
+++ login.js
@@ -4,12 +4,23 @@ let Email = [];
 
 function SUBMIT () {
     alert("Email or Password is empty");
-}
-
 var names = document.getElementById("name").Value;
 var names = document.getElementById("password").Value;
 var names = document.getElementById("Email").Value;
+}
 
-Email.innerHTML += "<p>" + Password + ","  + names + "</p>";
+if (Password == "") {
+    alert("item name cannot be empty");
+    return;
+}
+
+if (Email == 0) {
+    alert("price cannot be empty 0");
+    return;
+}
+
+names.push({name: names, Password: Password});
+
+varname.innerHTML ="";
 
 
