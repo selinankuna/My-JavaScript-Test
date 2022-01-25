0680047d259b6525831f80285991ecdb59e2f5ea
unknown <selinankunas@gmail.com>
2022-01-25 11:08:13 +0200

    function added

--- login.js
+++ login.js
@@ -1,6 +1,15 @@
 let names = [];
 let Password = [];
+let Email = [];
 
 function SUBMIT () {
     alert("Email or Password is empty");
 }
+
+var names = document.getElementById("name").Value;
+var names = document.getElementById("password").Value;
+var names = document.getElementById("Email").Value;
+
+Email.innerHTML += "<p>" + Password + ","  + names + "</p>";
+
+
